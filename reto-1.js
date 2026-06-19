78029f6bdbfcced107888e6a23d630cdb9e965fe
Nayaret Orellana <nayaret.orellanag@gmail.com>
2026-06-18 22:28:59 -0400

    practicas arreglos y for

---
+++ reto-1.js
@@ -0,0 +1,17 @@
+// 
+
+let mochila = ["cuerda", "linterna", "mapa", "brújula"];
+
+// Muestra primer objeto
+console.log("Primer objeto: ", mochila[0]);
+
+// ultimo objeto 
+console.log("ultimo objeto: ", mochila[mochila.length - 1]);
+
+// cuantos objetos hay 
+console.log("total:", mochila.length);
+
+// cambia mapa x gps
+mochila[2] = "GPS";
+
+console.log("Mochila", mochila);
