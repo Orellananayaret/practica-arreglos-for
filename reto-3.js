c4998062ee8290b05a0de568fa239c0baba0049f
Nayaret Orellana <nayaret.orellanag@gmail.com>
2026-06-18 22:58:04 -0400

    reto 3

---
+++ reto-3.js
@@ -0,0 +1,8 @@
+// 
+let jugadores = ["Sora", "Kai", "Luna", "Max"];
+
+// recorre for 
+
+for (let i = 0; i < jugadores.length; i++) {
+    console.log("Jugador " + (i + 1) + ": " + jugadores[i]);
+}
